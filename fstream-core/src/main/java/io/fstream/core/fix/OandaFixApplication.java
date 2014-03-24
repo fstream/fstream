584adccc6db4863919958ad3ceb015e925ce15a5
Bob Tiernay <btiernay@oicr.on.ca>
2014-03-23 21:21:04 -0400

    Updated Maven repo, QuickFIX/J versions, added Logback and config.

--- fstream-core/src/main/java/io/fstream/core/fix/OandaFixApplication.java
+++ fstream-core/src/main/java/io/fstream/core/fix/OandaFixApplication.java
@@ -8,78 +8,77 @@
  */
 package io.fstream.core.fix;
 
-import quickfix.*;
-import quickfix.Message.Header;
-import quickfix.field.BeginString;
-import quickfix.field.HeartBtInt;
-import quickfix.field.Password;
-import quickfix.field.ResetSeqNumFlag;
-import quickfix.field.Username;
+import lombok.extern.slf4j.Slf4j;
+import quickfix.Application;
+import quickfix.DoNotSend;
+import quickfix.FieldNotFound;
+import quickfix.IncorrectDataFormat;
+import quickfix.IncorrectTagValue;
+import quickfix.Message;
+import quickfix.MessageCracker;
+import quickfix.RejectLogon;
+import quickfix.SessionID;
+import quickfix.UnsupportedMessageType;
 import quickfix.fix44.Logon;
 import quickfix.fix44.NewOrderSingle;
 import quickfix.fix44.SecurityDefinition;
 
+@Slf4j
 public class OandaFixApplication extends MessageCracker implements Application {
 
 	@Override
-    public void fromAdmin(Message arg0, SessionID arg1) throws FieldNotFound,
-            IncorrectDataFormat, IncorrectTagValue, RejectLogon {
-        System.out.println("Successfully called fromAdmin for sessionId : "
-                + arg0);
-    }
- 
-    @Override
-    public void fromApp(Message arg0, SessionID arg1) throws FieldNotFound,
-            IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
-        System.out.println("Successfully called fromApp for sessionId : "
-                + arg0);
-    }
- 
-    @Override
-    public void onCreate(SessionID arg0) {
-        System.out.println("Successfully called onCreate for sessionId : "
-                + arg0);    
-    }
- 
-    @Override
-    public void onLogon(SessionID arg0) {
-        System.out.println("Successfully logged on for sessionId : " + arg0);
-    }
- 
-    @Override
-    public void onLogout(SessionID arg0) {
-        System.out.println("Successfully logged out for sessionId : " + arg0);
-    }
- 
-    @Override
-    public void toAdmin(Message message, SessionID sessionId) {
-        System.out.println("Inside toAdmin");
-    }
- 
-    @Override
-    public void toApp(Message arg0, SessionID arg1) throws DoNotSend {
-        System.out.println("Message : " + arg0 + " for sessionid : " + arg1);
-    }
- 
-    public void onMessage(NewOrderSingle message, SessionID sessionID)
-            throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
-        System.out.println("Inside onMessage for New Order Single");
-        super.onMessage(message, sessionID);
-    }
- 
-   
-    public void onMessage(SecurityDefinition message, SessionID sessionID)
-            throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
-        System.out.println("Inside onMessage for SecurityDefinition");
-        super.onMessage(message, sessionID);
-    }
- 
-   
-    public void onMessage(Logon message, SessionID sessionID)
-            throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
-        System.out.println("Inside Logon Message");
-        super.onMessage(message, sessionID);
-    }
+	public void fromAdmin(Message arg0, SessionID arg1) throws FieldNotFound,
+			IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+		log.info("Successfully called fromAdmin for sessionId: {}", arg0);
+	}
 
+	@Override
+	public void fromApp(Message arg0, SessionID arg1) throws FieldNotFound,
+			IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+		log.info("Successfully called fromApp for sessionId: {}", arg0);
+	}
+
+	@Override
+	public void onCreate(SessionID arg0) {
+		log.info("Successfully called onCreate for sessionId: {}", arg0);
+	}
+
+	@Override
+	public void onLogon(SessionID arg0) {
+		log.info("Successfully logged on for sessionId: {}", arg0);
+	}
+
+	@Override
+	public void onLogout(SessionID arg0) {
+		log.info("Successfully logged out for sessionId : " + arg0);
+	}
+
+	@Override
+	public void toAdmin(Message message, SessionID sessionId) {
+		log.info("Inside toAdmin");
+	}
+
+	@Override
+	public void toApp(Message arg0, SessionID arg1) throws DoNotSend {
+		log.info("Message : {} for sessionid : {}", arg0, arg1);
+	}
+
+	public void onMessage(NewOrderSingle message, SessionID sessionID)
+			throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
+		log.info("Inside onMessage for New Order Single");
+		super.onMessage(message, sessionID);
+	}
+
+	public void onMessage(SecurityDefinition message, SessionID sessionID)
+			throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
+		log.info("Inside onMessage for SecurityDefinition");
+		super.onMessage(message, sessionID);
+	}
+
+	public void onMessage(Logon message, SessionID sessionID)
+			throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
+		log.info("Inside Logon Message");
+		super.onMessage(message, sessionID);
+	}
 
 }
--- fstream-core/src/main/java/io/fstream/core/fix/TestQuickFixJConnectivity.java
+++ fstream-core/src/main/java/io/fstream/core/fix/TestQuickFixJConnectivity.java
@@ -8,69 +8,88 @@
  */
 package io.fstream.core.fix;
 
-import quickfix.*;
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+import quickfix.ConfigError;
+import quickfix.DefaultMessageFactory;
+import quickfix.FileStoreFactory;
 import quickfix.Message.Header;
+import quickfix.SLF4JLogFactory;
+import quickfix.Session;
+import quickfix.SessionID;
+import quickfix.SessionNotFound;
+import quickfix.SessionSettings;
+import quickfix.SocketInitiator;
 import quickfix.field.Account;
 import quickfix.field.BeginString;
-import quickfix.field.HeartBtInt;
 import quickfix.field.Password;
 import quickfix.field.ResetSeqNumFlag;
 import quickfix.field.Username;
 import quickfix.fix44.Logon;
 
-
+@Slf4j
 public class TestQuickFixJConnectivity {
-    public static void main(String[] args) {
+	
+	/**
+	 * Config.
+	 * <p>
+	 * Set on the command line / launcher using {@code -Dfix.username=xxx -Dfix.password=yyy}
+	 */
+	private static final String FIX_USERNAME = System.getProperty("fix.username");
+	private static final String FIX_PASSWORD = System.getProperty("fix.password");
+	
+    public static void main(String... args) {
         SocketInitiator socketInitiator = null;
         try {
-            SessionSettings sessionSettings = new SessionSettings("/Users/bdevani/Documents/eclipse-workspace/fstream/fstream-core/src/main/java/io/fstream/core/fix/quickfix2.cfg");
-            Application application = new OandaFixApplication();
-            FileStoreFactory fileStoreFactory = new FileStoreFactory(sessionSettings);
-            FileLogFactory logFactory = new FileLogFactory(sessionSettings);
-            MessageFactory messageFactory = new DefaultMessageFactory();
-            socketInitiator = new SocketInitiator(application,
-                    fileStoreFactory, sessionSettings, logFactory,
-                    messageFactory);
+            socketInitiator = createSocketInitiator();
             socketInitiator.start();
+            
             Thread.sleep(1000);
-            SessionID sessionId = socketInitiator.getSessions().get(0);
-            System.out.println("session id " + sessionId);
+            val sessionId = socketInitiator.getSessions().get(0);
+            log.info("session id " + sessionId);
             sendLogonRequest(sessionId);
+            
             int i = 0;
-            System.out.println("Is loggedOn " + socketInitiator.isLoggedOn());
+            val maxAttempts = 5;
             do {
                 try {
+                    log.info("isLoggedOn: {}", socketInitiator.isLoggedOn());
                     Thread.sleep(1000);
-                    System.out.println("is logged " + socketInitiator.isLoggedOn());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 i++;
-            } while ((!socketInitiator.isLoggedOn()) && (i < 10));
-           
-        } catch (ConfigError e) {
-            e.printStackTrace();
-        } catch (SessionNotFound e) {
-           e.printStackTrace();
-        } catch (Exception exp) {
-            exp.printStackTrace();
+            } while ((!socketInitiator.isLoggedOn()) && (i < maxAttempts));
+        } catch (Throwable t) {
+        	log.error("Exception logging on: ", t);
         } finally {
             if (socketInitiator != null) {
                 socketInitiator.stop(true);
             }
         }
     }
-    private static void sendLogonRequest(SessionID sessionId)
-            throws SessionNotFound {
+	private static SocketInitiator createSocketInitiator() throws ConfigError {
+		val application = new OandaFixApplication();
+		val sessionSettings = new SessionSettings("quickfix2.cfg");
+		val fileStoreFactory = new FileStoreFactory(sessionSettings);
+		val logFactory = new SLF4JLogFactory(sessionSettings);
+		val messageFactory = new DefaultMessageFactory();
+		
+		return new SocketInitiator(application, fileStoreFactory, sessionSettings, logFactory, messageFactory);
+	}
+	
+    private static void sendLogonRequest(SessionID sessionId)throws SessionNotFound {
         Logon logon = new Logon();
         Header header = logon.getHeader();
         header.setField(new BeginString("FIX.4.4"));
-        logon.setField(new Username("xxx"));
-        logon.setField(new Password("yyy"));
+        logon.setField(new Username(FIX_USERNAME));
+        logon.setField(new Password(FIX_PASSWORD));
         logon.setField(new Account(""));
         logon.set(new ResetSeqNumFlag(true));
-        System.out.println(logon.toString());
-        boolean sent = Session.sendToTarget(logon, sessionId);
-        System.out.println("Logon Message Sent : " + sent);
+        log.info("logon request: {}", logon.toString());
+        
+        val sent = Session.sendToTarget(logon, sessionId);
+        log.info("Logon Message Sent : {}", sent);
     }
+    
 }
