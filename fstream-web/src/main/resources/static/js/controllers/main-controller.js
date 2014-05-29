f16b1337736226a7f103a8a5cbf9280bc7a4e80c
Bob Tiernay <btiernay@oicr.on.ca>
2014-05-29 08:03:02 -0400

    Internal refactoring.

--- fstream-web/src/main/resources/static/js/controllers/main-controller.js
+++ fstream-web/src/main/resources/static/js/controllers/main-controller.js
@@ -1,69 +1,70 @@
 angular.module('FStreamApp.controllers').controller('mainController', function($scope, _, configService, eventService) {
-	
-	//
-	// Methods
-	//
-	
-	var queue = function(a, value, limit) {
-			return a.length >= limit ? a.pop() : a.unshift(value);
-		}, 
+	function registerEvents() {
+		// Events
+		$scope.$on('connected', function(e) {
+			$scope.connected = true;
+		});
+		$scope.$on('disconnected', function(e) {
+			$scope.connected = false;
+		});
 		
-		reset = function() {
-			$scope.rates = [];
-			$scope.alerts = [];
-			$scope.commands = [];
-		},
-		
-		register = function() {
-			
-			// Scope
-			$scope.connect = function() {
-				eventService.connect();
-			};
-			$scope.disconnect = function() {
-				eventService.disconnect();
-			}
-			$scope.register = function() {
-				eventService.register($scope.instrument);
-			}
-			
-			// Events
-			$scope.$on('connected', function(e) {
-				$scope.connected = true;
-				reset();
-			});
-			$scope.$on('disconnected', function(e) {
-				$scope.connected = false;
-			});
-			
-		    $scope.$on('rate', function(e, rate) {
-		    	queue($scope.rates, rate, 50);
-		    });
-		    $scope.$on('alert', function(e, alert) {
-		    	queue($scope.alerts, alert, 50);
-		    });
-		    $scope.$on('command', function(e, command) {
-		    	queue($scope.commands, command, 50);
-		    });
-		    
-		};
+		// TODO: Rename
+		$scope.$on('rate', function(e, rate) {
+			queueEvent($scope.rates, rate, 50);
+		});
+		$scope.$on('alert', function(e, alert) {
+			queueEvent($scope.alerts, alert, 50);
+		});
+		$scope.$on('command', function(e, command) {
+			queueEvent($scope.commands, command, 50);
+		});
+	};
 	
+	function connect() {
+		configService.getConfig().then(updateInstruments);
+		initScope();
+		resetModel();
 		
-	// Bootstrap configuration
-	configService.getConfig().then(function(instruments) {
-	  $scope.instruments = instruments;
-	  
-	  $scope.tickCharts = new Array(instruments.length);
-	  for (var i = 0; i < instruments.length; i++) {
-		  $scope.tickCharts[i] = {
-			index: i,
-			symbol: instruments[i]
-		  };
-	  }
-	});
-
+		eventService.connect();
+	}
+	
+	function disconnect() {
+		eventService.disconnect();
+	}
+	
+	function initScope() {
+		$scope.connected = false;
+		$scope.instruments = [];
+		$scope.connect = connect;
+		$scope.disconnect = disconnect;
+		$scope.register = registerInstrument;
+	}
+		
+	function resetModel() {
+		$scope.rates = [];
+		$scope.alerts = [];
+		$scope.commands = [];
+	}
+	
+	function registerInstrument() {
+		eventService.register($scope.instrument);
+	}
+		
+	function updateInstruments(instruments) {
+		$scope.instruments = instruments;
+		$scope.tickCharts = _.map(instruments, function(symbol, i) {
+			return {
+				index: i,
+				symbol: symbol
+		    };
+		});
+	};
+		
+	function queueEvent(a, value, limit) {
+		return a.length >= limit ? a.pop() : a.unshift(value);
+	};
+		
 	// Initialize
-	$scope.connected = false;
-	reset();
-	register();
+	registerEvents();
+	connect();
 });
