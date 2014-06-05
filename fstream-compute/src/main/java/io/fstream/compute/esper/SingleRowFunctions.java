074cf562d31b9876f6be5e336d6cca687d047d40
Bob Tiernay <btiernay@oicr.on.ca>
2014-06-05 08:15:04 -0400

    Added example esper singlerow-function.

---
+++ fstream-compute/src/main/java/io/fstream/compute/esper/SingleRowFunctions.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2014 fStream. All Rights Reserved.
+ *
+ * Project and contact information: https://bitbucket.org/fstream/fstream
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package io.fstream.compute.esper;
+
+import static lombok.AccessLevel.PRIVATE;
+import lombok.NoArgsConstructor;
+
+/**
+ * Esper singlerow-functions.
+ * 
+ * @See http://esper.codehaus.org/esper-5.0.0/doc/reference/en-US/html_single/index.html#custom-singlerow-function
+ */
+@NoArgsConstructor(access = PRIVATE)
+public class SingleRowFunctions {
+
+  public static String test() {
+    return "Passed!";
+  }
+
+}
