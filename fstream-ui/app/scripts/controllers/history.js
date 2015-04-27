90f5afb978cce82c625856aed802bc1a07244ca9
Bob Tiernay <btiernay@oicr.on.ca>
2015-04-27 19:57:23 -0400

    Added basic symbol filtering.

--- fstream-ui/app/scripts/controllers/history.js
+++ fstream-ui/app/scripts/controllers/history.js
@@ -13,15 +13,24 @@ angular
     .module('homer')
     .factory('historyService', function($http) {
    return {
-        getHistory: function() {
-             return $http.get('http://localhost:8086/db/ticks/series', {params: {u: 'root', p: 'root', q: 'select * from /.*/ LIMIT 10'}
-})
+        getHistory: function(symbol, start, end, limit) {
+            symbol = (symbol || '.*').replace('/', '\\/');
+            limit = limit || 50;
+            return $http.get('http://localhost:8086/db/ticks/series', {
+                params: {u: 'root', p: 'root', q: 'select * from /' + symbol + '/ LIMIT ' + limit}
+            });
         }
-   }
+   };
 });
 
 function historyCtrl($scope, historyService) {
     historyService.getHistory().then(function(result) {
         $scope.history = result.data;
     });
+    
+    $scope.submit = function() {
+         historyService.getHistory($scope.symbol).then(function(result) {
+            $scope.history = result.data;
+        });
+    }
 }
