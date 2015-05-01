2d7af5dfbc77ee3e712a558ef734f397e87d7abd
Bob Tiernay <btiernay@oicr.on.ca>
2015-04-30 21:31:19 -0400

    Added basic navigator to chart.

--- fstream-ui/app/scripts/directives/metric-chart-directive.js
+++ fstream-ui/app/scripts/directives/metric-chart-directive.js
@@ -1,84 +1,105 @@
 angular.module('homer').directive('metricChart', function() {
-	Highcharts.setOptions({
-		global : {
-			useUTC : false
-		}
-	});
-	
-	return {
-		restrict : 'E',
-		scope: {
-	      options: '='
-	    },
-	    replace: true,
-		template : '<div class="metric-chart"></div>',
-		link: function($scope, $element, $attr){
-			var chart,
-			    index = $scope.options.index,
-			    colors = Highcharts.getOptions().colors,
-			    color = '#62cb31',
-			    opacity =  0.5,
-			    size = 50,
-			    enabled = true;
-			
-			chart = new Highcharts.Chart({
-		        chart: {
-		            renderTo: $element[0],
-		            height: 325,
-		            type: 'area',
-		            animation: false
-		        },
-		        
-				credits: {
-					enabled: false
-				},
-		        
-		        title: {
-		        	text: $scope.options.title
-		        },
-		        
-		        yAxis: {
-		        	title: {
-		        		text: $scope.options.units
-		        	},
-	                alternateGridColor: '#FDFDfD'
-		        },
-                
-	            xAxis: {
-	                type: 'datetime',
+    Highcharts.setOptions({
+        global : {
+            useUTC : false
+        }
+    });
+
+    return {
+        restrict : 'E',
+        scope: {
+            options: '='
+        },
+        replace: true,
+        template : '<div class="metric-chart"></div>',
+        link: function($scope, $element, $attr){
+            var chart,
+                index = $scope.options.index,
+                colors = Highcharts.getOptions().colors,
+                color = '#62cb31',
+                opacity =  0.5,
+                size = 50,
+                enabled = true;
+
+            chart = $($element[0]).highcharts('StockChart', {
+                chart: {
+                    height: 325,
+                    type: 'area',
+                    animation: false
+                },
+
+                credits: {
+                    enabled: false
+                },
+
+                title: {
+                    text: $scope.options.title
+                },
+
+                yAxis: {
+                    title: {
+                        text: $scope.options.units
+                    },
+                    alternateGridColor: '#FDFDfD'
+                },
+
+                xAxis: {
+                    type: 'datetime',
                     gridLineWidth:'1px'
-	            },
-	            
-		        tooltip: {
-		            crosshairs: [true, true],
-		            shared: true
-		        },              
+                },
+
+                tooltip: {
+                    crosshairs: [true, true],
+                    shared: true
+                }, 
+
+                rangeSelector : {
+                    buttons : [{
+                        type : 'hour',
+                        count : 1,
+                        text : '1h'
+                    }, {
+                        type : 'day',
+                        count : 1,
+                        text : '1D'
+                    }, {
+                        type : 'all',
+                        count : 1,
+                        text : 'All'
+                    }],
+                    selected : 1,
+                    inputEnabled : false
+                },
                 
-				series: [{
-			    	name: $scope.options.name,
-			    	data: [],
-			    	zIndex: 1,
-			    	color: color,
-			    	lineColor: color,
-			    	marker: {
-			    		fillColor: 'white',
-			    		lineWidth: 2,
-			    		radius: 3,
-			    		lineColor: color
-			    	}
-				}]
-			});
-			
-		    $scope.$on('metric', function(e, metric) {
-		    	if (metric.id !== $scope.options.id) {
-		    		return;
-		    	}
-		    	
-	    		var shift = chart.series[0].data.length >= size,
-    		    	animate = false;
-	    		
-	    		chart.series[0].addPoint([metric.dateTime, metric.data.count], enabled, shift, animate);
-		    });
-		}
-	}
+                navigator: {
+                    maskFill: 'rgba(255, 255, 255, 0.45)'
+                },
+                
+                series: [{
+                    name: $scope.options.name,
+                    data: [],
+                    zIndex: 1,
+                    color: color,
+                    lineColor: color,
+                    marker: {
+                        fillColor: 'white',
+                        lineWidth: 2,
+                        radius: 3,
+                        lineColor: color
+                    }
+                }]
+            }).highcharts();
+
+            $scope.$on('metric', function(e, metric) {
+                if (metric.id !== $scope.options.id) {
+                    return;
+                }
+
+                var shift = chart.series[0].data.length >= size,
+                    animate = false;
+
+                chart.series[0].addPoint([metric.dateTime, metric.data.count], enabled, shift, animate);
+            });
+        }
+    }
 })
