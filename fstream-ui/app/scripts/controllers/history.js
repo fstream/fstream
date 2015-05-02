1dd5c4d8dbd33800cc35debb8b1ff315a36125ed
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-02 12:56:36 -0400

    Encapsulated tick in object that is controlled by history service.

--- fstream-ui/app/scripts/controllers/history.js
+++ fstream-ui/app/scripts/controllers/history.js
@@ -8,31 +8,14 @@ angular
    .module('fstream')
    .controller('historyCtrl', historyCtrl)
 
-
-angular
-   .module('fstream')
-   .factory('historyService', function($http) {
-   var config = {
-      databaseName: 'fstream-events'
-   }
-   return {
-      getHistory: function(symbol, start, end, limit) {
-         limit = limit || 10;
-         return $http.get('http://localhost:8086/db/' + config.databaseName + '/series', {
-            params: {u: 'root', p: 'root', q: 'SELECT * FROM "ticks" GROUP BY symbol LIMIT ' + limit}
-         });
-      }
-   };
-});
-
 function historyCtrl($scope, historyService) {
-   getHistory();
+   updateHistory();
 
-   $scope.submit = getHistory();
+   $scope.updateHistory = updateHistory;
 
-   function getHistory() {
-      historyService.getHistory($scope.symbol).then(function(result) {
-         $scope.history = result.data[0];
+   function updateHistory() {
+      historyService.getHistory({symbol: $scope.symbol}).then(function(history) {
+         $scope.history = history
       });
    }
 }
