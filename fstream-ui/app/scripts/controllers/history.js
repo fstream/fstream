556114e55e24b7f329960fab067913374ceea907
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-07 07:18:34 -0400

    Implemented table filtering.

--- fstream-ui/app/scripts/controllers/history.js
+++ fstream-ui/app/scripts/controllers/history.js
@@ -5,10 +5,12 @@
       .module('fstream')
       .controller('historyCtrl', historyCtrl);
 
-   historyCtrl.$inject = ['$scope', 'historyService'];
+   historyCtrl.$inject = ['$scope', '$filter', 'historyService'];
 
-   function historyCtrl($scope, historyService) {
+   function historyCtrl($scope, $filter, historyService) {
       $scope.updateHistory = updateHistory;
+      $scope.updateTimeRange = updateTimeRange;
+      $scope.updateSymbol = updateSymbol;
       $scope.symbols = {
          selected: []
       };
@@ -23,15 +25,25 @@
       function updateHistory() {
          var params = {
             symbol: $scope.symbols.selected.length && $scope.symbols.selected[0].name,
-            startTime: $scope.startTime,
-            endTime: $scope.endTime
-         };
-
+            startTime: $scope.startTime && moment($scope.startTime, "YYYY-MM-DD hh:mm:ss").unix(),
+            endTime: $scope.endTime && moment($scope.endTime, "YYYY-MM-DD hh:mm:ss").unix() + 1
+         }
+         
          historyService.getHistory(params).then(function (history) {
             $scope.history = history;
          });
       }
 
+      function updateTimeRange(time) {
+         $scope.startTime = $scope.endTime = $filter('date')(time, 'yyyy-MM-dd HH:mm:ss');
+      }      
+
+      function updateSymbol(name) {
+         $scope.symbols = {
+            selected: [{name: name}]
+         }
+      }     
+      
       function getAvailableSymbols() {
          historyService.getSymbols().then(function (symbols) {
             $scope.availableSymbols = symbols;
