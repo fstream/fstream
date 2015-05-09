643865e1188460272d5d47f915451b42dfb68845
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-09 18:04:58 -0400

    Added autocomplete support for active symbols.

--- fstream-ui/app/scripts/controllers/admin-controller.js
+++ fstream-ui/app/scripts/controllers/admin-controller.js
@@ -1,12 +1,25 @@
 (function () {
    'use strict';
-   
+
    angular
       .module('fstream')
       .controller('adminController', adminController);
 
-   adminController.$inject = ['$scope'];
-   
-   function adminController($scope) {
+   adminController.$inject = ['$scope', 'lodash'];
+
+   function adminController($scope, _) {
+      $scope.aceLoaded = function (editor) {
+         // Options
+         editor.completers.push({
+            getCompletions: function(editor, session, pos, prefix, callback) {
+               var activeSymbols = _.map($scope.state.symbols, function(symbol) {
+                  return {value: "'" + symbol + "'", score: 1000, meta: "symbol"}
+               });
+               
+               callback(null, activeSymbols);
+            }
+         });
+
+      };
    }
 })();
