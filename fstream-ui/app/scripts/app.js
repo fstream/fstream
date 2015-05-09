3ce7e428e9c79f3db3dd387c9ec044c7689fc425
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-09 14:45:11 -0400

    Integrated basic editor.

--- fstream-ui/app/scripts/app.js
+++ fstream-ui/app/scripts/app.js
@@ -4,27 +4,28 @@
  *
  */
 (function () {
-    angular.module('fstream', [
-        'ui.router',                // Angular flexible routing
-        'ngSanitize',               // Angular-sanitize
-        'ui.bootstrap',             // AngularJS native directives for Bootstrap
-        'angular-flot',             // Flot chart
-        'angles',                   // Chart.js
-        'angular-peity',            // Peity (small) charts
-        'cgNotify',                 // Angular notify
-        'angles',                   // Angular ChartJS
-        'ngAnimate',                // Angular animations
-        'ui.map',                   // Ui Map for Google maps
-        'ui.calendar',              // UI Calendar
-        'summernote',               // Summernote plugin
-        'ngGrid',                   // Angular ng Grid
-        'ui.tree',                  // Angular ui Tree
-        'bm.bsTour',                // Angular bootstrap tour
-        'datatables',               // Angular datatables plugin
-        'xeditable',                // Angular-xeditable
-        'ui.select',                // AngularJS ui-select
-        'ngLodash',
-        'prism'
-    ])
+   angular.module('fstream', [
+      'ui.router',                // Angular flexible routing
+      'ngSanitize',               // Angular-sanitize
+      'ui.bootstrap',             // AngularJS native directives for Bootstrap
+      'angular-flot',             // Flot chart
+      'angles',                   // Chart.js
+      'angular-peity',            // Peity (small) charts
+      'cgNotify',                 // Angular notify
+      'angles',                   // Angular ChartJS
+      'ngAnimate',                // Angular animations
+      'ui.map',                   // Ui Map for Google maps
+      'ui.calendar',              // UI Calendar
+      'summernote',               // Summernote plugin
+      'ngGrid',                   // Angular ng Grid
+      'ui.tree',                  // Angular ui Tree
+      'bm.bsTour',                // Angular bootstrap tour
+      'datatables',               // Angular datatables plugin
+      'xeditable',                // Angular-xeditable
+      'ui.select',                // AngularJS ui-select
+      'ngLodash',
+      'prism',
+      'ui.ace'
+   ])
 })();
 
