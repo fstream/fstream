c6282a0f3e2a7979d23ca250b84c8d8ea2cc3ff4
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-10 19:52:54 -0400

    Added success message (even if it fails...).

--- fstream-ui/app/scripts/controllers/admin-controller.js
+++ fstream-ui/app/scripts/controllers/admin-controller.js
@@ -5,30 +5,41 @@
       .module('fstream')
       .controller('adminController', adminController);
 
-   adminController.$inject = ['$scope', 'lodash', 'eventService'];
+   adminController.$inject = ['$scope', 'lodash', 'sweetAlert', 'eventService'];
 
-   function adminController($scope, _, eventService) {
+   function adminController($scope, _, sweetAlert, eventService) {
       $scope.alert = {
          name: '',
          description: '',
          statement: ''
       };
-      
+
       $scope.aceLoaded = function (editor) {
          // Options
          editor.completers.push({
-            getCompletions: function(editor, session, pos, prefix, callback) {
-               var activeSymbols = _.map($scope.state.symbols, function(symbol) {
-                  return {value: "'" + symbol + "'", score: 1000, meta: "symbol"}
+            getCompletions: function (editor, session, pos, prefix, callback) {
+               var activeSymbols = _.map($scope.state.symbols, function (symbol) {
+                  return {
+                     value: "'" + symbol + "'",
+                     score: 1000,
+                     meta: "symbol"
+                  }
                });
-               
+
                callback(null, activeSymbols);
             }
          });
       };
-      
-      $scope.registerAlert = function(alert) {
-         eventService.register(alert);
+
+      $scope.registerAlert = function (alert) {
+         eventService.register(alert).then(function(){
+            sweetAlert.swal({
+               title: "Registration Successful",
+               text: 'Alert "' + alert.name + '" was registered',
+               type: "success",
+               confirmButtonColor: "#62cb31"
+            })
+         });
       }
    }
 })();
--- fstream-ui/app/scripts/services/event-service.js
+++ fstream-ui/app/scripts/services/event-service.js
@@ -5,9 +5,9 @@
       .module('fstream')
       .factory('eventService', eventService);
 
-   eventService = ['$rootScope', '$timeout'];
+   eventService = ['$rootScope', '$timeout', '$q'];
 
-   function eventService($rootScope, $timeout) {
+   function eventService($rootScope, $timeout, $q) {
       var stompClient,
           publishEvent = function (eventName, frame){
              $timeout(function() {
@@ -54,6 +54,9 @@
 
          register: function(alert){
             stompClient.send("/web/register", {}, angular.toJson(alert));
+            var deferred = $q.defer();
+            deferred.resolve({success: true});
+            return deferred.promise;
          }
 
       } 
