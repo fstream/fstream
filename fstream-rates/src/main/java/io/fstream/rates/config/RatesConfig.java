551bf323ab9e3c73ba8535c28166616403924ea6
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-23 09:13:38 -0400

    Removed port.

--- fstream-rates/src/main/java/io/fstream/rates/config/RatesConfig.java
+++ fstream-rates/src/main/java/io/fstream/rates/config/RatesConfig.java
@@ -9,15 +9,8 @@
 
 package io.fstream.rates.config;
 
-import static java.util.concurrent.TimeUnit.MINUTES;
 import io.fstream.core.config.CoreConfig;
-import io.fstream.core.util.Port;
 
-import javax.annotation.PostConstruct;
-
-import lombok.SneakyThrows;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -26,16 +19,5 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 public class RatesConfig extends CoreConfig {
-
-  @Value("${zk.host}")
-  String host;
-  @Value("${zk.port}")
-  int port;
-
-  @PostConstruct
-  @SneakyThrows
-  public void init() {
-    new Port(host, port).waitFor(1L, MINUTES);
-  }
 
 }
