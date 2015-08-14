ed22aa870fdd4d195837d52ab08260814386f91f
Baiju Devani <b.devani@gmail.com>
2015-08-14 11:49:02 -0400

    added bookside test for add/cancel orders

--- fstream-simulate/src/test/java/io/fstream/simulate/util/BookSideTest.java
+++ fstream-simulate/src/test/java/io/fstream/simulate/util/BookSideTest.java
@@ -13,6 +13,7 @@ import static io.fstream.core.model.event.Order.OrderSide.ASK;
 import static io.fstream.core.model.event.Order.OrderType.LIMIT_ADD;
 import static org.assertj.core.api.Assertions.assertThat;
 import io.fstream.core.model.event.Order;
+import io.fstream.core.model.event.Order.OrderType;
 import lombok.val;
 
 import org.joda.time.DateTime;
@@ -41,6 +42,27 @@ public class BookSideTest {
     assertThat(bookSide.calculateDepth()).isEqualTo(0);
     assertThat(bookSide.calculateOrderCount()).isEqualTo(0);
 
+  }
+
+  @Test
+  public void testOrderPutAndCancel() {
+    // Setup
+    val orderAmount = 100;
+    val dateTime = DateTime.now();
+    val order1 = new Order(ASK, LIMIT_ADD, dateTime, 1, "Fidelity", "RY", orderAmount, 50.0f, "trader1");
+    val order2 = new Order(ASK, LIMIT_ADD, dateTime, 2, "ETrade", "RY", 200, 50.0f, "trader2");
+
+    val bookSide = new BookSide(ASK);
+    bookSide.addOrder(order1);
+    assertThat(bookSide.getDepth()).isEqualTo(order1.getAmount());
+    bookSide.addOrder(order2);
+    assertThat(bookSide.calculateDepth()).isEqualTo(order1.getAmount() + order2.getAmount());
+
+    val order3 =
+        new Order(ASK, OrderType.LIMIT_CANCEL, dateTime, 1, "Fidelity", "RY", orderAmount, 50.0f, "trader1");
+    bookSide.removeOrder(order3);
+    assertThat(bookSide.calculateDepth()).isEqualTo(order2.getAmount());
+    assertThat(bookSide.getDepth()).isEqualTo(order2.getAmount());
   }
 
 }
