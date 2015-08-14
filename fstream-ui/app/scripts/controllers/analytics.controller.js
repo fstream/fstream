1028e7c3e243856e832255ca4499723877221ecc
Bob Tiernay <btiernay@oicr.on.ca>
2015-08-13 20:11:54 -0400

    Fixed compilation errors.

--- fstream-ui/app/scripts/controllers/analytics.controller.js
+++ fstream-ui/app/scripts/controllers/analytics.controller.js
@@ -1,23 +1,23 @@
 /* 
  * Copyright (c) 2015 fStream. All Rights Reserved.
- * 
+ *
  * Project and contact information: https://bitbucket.org/fstream/fstream
- * 
+ *
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * Proprietary and confidential.
  */
 
 (function () {
    'use strict';
-   
+
    angular
       .module('fstream')
       .controller('analyticsController', analyticsController);
 
    analyticsController.$inject = ['$scope', 'analyticsService'];
-   
+
    function analyticsController($scope, analyticsService) {
-      analyticsService.executeQuery({}).then(function(results) {
+      analyticsService.executeQuery({}).then(function (results) {
          $scope.results = results;
       });
    }
