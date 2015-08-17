d350ccab93de10ac7e0c628595dc95d5333aeb73
Bob Tiernay <btiernay@oicr.on.ca>
2015-08-16 22:07:31 -0400

    Dynamic reading.

--- fstream-simulate/src/main/java/io/fstream/simulate/core/Replayer.java
+++ fstream-simulate/src/main/java/io/fstream/simulate/core/Replayer.java
@@ -10,26 +10,20 @@
 package io.fstream.simulate.core;
 
 import static com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE;
-import static io.fstream.core.model.event.EventType.ORDER;
-import static io.fstream.core.model.event.EventType.QUOTE;
-import static io.fstream.core.model.event.EventType.TRADE;
 import io.fstream.core.model.event.AbstractEvent;
 import io.fstream.core.model.event.Event;
 import io.fstream.core.model.event.EventType;
-import io.fstream.core.model.event.Order;
-import io.fstream.core.model.event.Quote;
-import io.fstream.core.model.event.Snapshot;
-import io.fstream.core.model.event.Trade;
 import io.fstream.simulate.routes.PublishRoutes;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 import javax.annotation.PostConstruct;
 
-import lombok.Cleanup;
+import lombok.SneakyThrows;
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
@@ -43,8 +37,12 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.AbstractExecutionThreadService;
 
+/**
+ * Replays event streams from a specified directory of JSON files at the specified speed.'
+ */
 @Slf4j
 @Component
 @Profile("replay")
@@ -63,81 +61,105 @@ public class Replayer extends AbstractExecutionThreadService {
   @Value("${simulate.file.speed}")
   private float speed;
 
+  /**
+   * Dependencies.
+   */
   @Autowired
   private ProducerTemplate template;
 
   @PostConstruct
   public void init() throws Exception {
-    log.info("Replaying...");
+    log.info("Replaying event streams...");
     startAsync();
   }
 
   @Override
   protected void run() throws Exception {
-    // Wait for route
+    // Wait for destination camel route (heuristic)
     Thread.sleep(5000);
 
-    @Cleanup
-    val trades = read("fstream-simulate-trades.json", Trade.class);
-    @Cleanup
-    val orders = read("fstream-simulate-orders.json", Order.class);
-    @Cleanup
-    val quotes = read("fstream-simulate-quotes.json", Quote.class);
-    @Cleanup
-    val snapshots = read("fstream-simulate-snapshots.json", Snapshot.class);
+    // Open streams
+    val eventStreams = openEventStreams();
 
-    val events = createEventQueue();
-    events.add(trades.next());
-    events.add(orders.next());
-    events.add(quotes.next());
-    events.add(snapshots.next());
+    // Initialize time ordered queue of blended events
+    val eventQueue = createEventQueue();
+    for (val stream : eventStreams.values()) {
+      eventQueue.add(stream.next());
+    }
 
-    long currentTime = events.peek().getDateTime().getMillis();
+    // Initialize time based on first event
+    long currentTime = eventQueue.peek().getDateTime().getMillis();
 
-    while (!events.isEmpty()) {
-      // Next event
-      val event = events.remove();
+    while (!eventQueue.isEmpty()) {
+      // Next event in time order
+      val event = eventQueue.remove();
+
+      //
+      // Schedule
+      //
 
       val eventTime = event.getDateTime().getMillis();
       if (eventTime > currentTime) {
-        // Modulate delay based on speed.
-        val delay = (eventTime - currentTime) / speed;
+        // Simulate time delay to make current
+        val delay = calculateDelay(currentTime, eventTime);
+        Thread.sleep(delay);
 
-        // Simulate time delay
-        Thread.sleep((long) delay);
+        // Advance time
         currentTime = eventTime;
       }
 
-      event.setDateTime(DateTime.now());
-      send(event);
+      // Make it current
+      ((AbstractEvent) event).setDateTime(DateTime.now());
 
-      if (event.getType() == TRADE && trades.hasNext()) {
-        events.add(trades.next());
-      }
-      if (event.getType() == ORDER && orders.hasNext()) {
-        events.add(orders.next());
-      }
-      if (event.getType() == QUOTE && quotes.hasNext()) {
-        events.add(quotes.next());
-      }
-      if (event.getType() == EventType.SNAPSHOT && snapshots.hasNext()) {
-        events.add(snapshots.next());
+      //
+      // Publish
+      //
+
+      publish(event);
+
+      // Replenish event of this type
+      val eventStream = eventStreams.get(event.getType());
+      if (eventStream.hasNext()) {
+        eventQueue.add(eventStream.next());
       }
     }
   }
 
-  private void send(Event event) {
+  private long calculateDelay(long currentTime, long eventTime) {
+    // Modulate delay based on speed.
+    return (long) ((eventTime - currentTime) / speed);
+  }
+
+  private void publish(Event event) {
     template.sendBody(PublishRoutes.PUBLISH_ENDPOINT, event);
   }
 
-  private static PriorityQueue<AbstractEvent> createEventQueue() {
-    return new PriorityQueue<AbstractEvent>(Comparator.<AbstractEvent, DateTime> comparing(e -> e.getDateTime()));
+  @SneakyThrows
+  private Map<EventType, MappingIterator<Event>> openEventStreams() {
+    val eventStreams = Maps.<EventType, MappingIterator<Event>> newHashMap();
+    for (val eventType : EventType.values()) {
+      val eventFile = new File(inputDir, getEventFileName(eventType));
+      if (eventFile.exists()) {
+        log.info("*** Reading {} from {}", eventType, eventFile);
+        eventStreams.put(eventType, openEventStream(eventFile));
+      }
+    }
+
+    return eventStreams;
   }
 
-  private <T> MappingIterator<T> read(String fileName, Class<T> eventType) throws JsonProcessingException, IOException {
-    val reader = MAPPER.reader(eventType);
+  private static String getEventFileName(EventType eventType) {
+    val name = eventType.name().toLowerCase() + "s";
+    return "fstream-simulate-" + name + ".json";
+  }
 
-    return reader.readValues(new File(inputDir, fileName));
+  private static PriorityQueue<Event> createEventQueue() {
+    // Prioritize by time
+    return new PriorityQueue<Event>(Comparator.<Event, DateTime> comparing(e -> e.getDateTime()));
+  }
+
+  private static MappingIterator<Event> openEventStream(File file) throws JsonProcessingException, IOException {
+    return MAPPER.reader(Event.class).readValues(file);
   }
 
 }
