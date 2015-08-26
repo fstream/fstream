f136d3f693c98245fa87e8902abb963205fdce31
Baiju Devani <b.devani@gmail.com>
2015-08-25 20:46:55 -0400

    add hyphen in agent name

--- fstream-simulate/src/main/java/io/fstream/simulate/actor/Broker.java
+++ fstream-simulate/src/main/java/io/fstream/simulate/actor/Broker.java
@@ -74,15 +74,15 @@ public class Broker extends BaseActor {
   }
 
   private ActorRef createRetailAgent(int i) {
-    return createAgent(RetailAgent.class, "ret" + i);
+    return createAgent(RetailAgent.class, "ret-" + i);
   }
 
   private ActorRef createInstitutionalAgent(int i) {
-    return createAgent(InstitutionalAgent.class, "inst" + i);
+    return createAgent(InstitutionalAgent.class, "inst-" + i);
   }
 
   private ActorRef createHftAgent(int i) {
-    return createAgent(HFTAgent.class, "hft" + i);
+    return createAgent(HFTAgent.class, "hft-" + i);
   }
 
   private ActorRef createAgent(Class<? extends Agent> agentClass, String name) {
